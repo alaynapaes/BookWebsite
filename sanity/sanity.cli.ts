1f4d6db748524cdd4492da75edf3ab46c6a15d81
Alayna Paes <alayna.paes@gmail.com>
2025-12-18 10:47:09 +0530

    trying changes

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -5,11 +5,5 @@ export default defineCliConfig({
     projectId: 'fcd3pz2x',
     dataset: 'production'
   },
-  deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/cli#auto-updates
-     */
-    autoUpdates: true,
-  }
+  studioHost: 'book-reviews'
 })
