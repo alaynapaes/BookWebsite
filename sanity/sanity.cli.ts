3359899e2b985e06e8b3b778605dae5e39fb1144
Alayna Paes <alayna.paes@gmail.com>
2025-12-20 12:19:36 +0530

    change reviewForm to review

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -5,5 +5,8 @@ export default defineCliConfig({
     projectId: 'fcd3pz2x',
     dataset: 'production'
   },
+  deployment: {
+    appId: 'nc77cclx7rzpp3viw1hvtf0o',
+  },
   studioHost: 'book-reviews'
 })
